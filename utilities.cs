ce33c682313e6892e01a466aa382209621793b89
Satou9 <122270657+Satou9@users.noreply.github.com>
2023-05-03 13:44:47 -0700

    Hanae

---
+++ utilities.cs
@@ -0,0 +1,69 @@
+/* utilities.css */
+
+:root {
+  --color-primary: #6f1e51;
+  --color-secondary: #f7dc6f;
+  --color-accent: #d35400;
+  --spacing-xs: 0.5rem;
+  --spacing-s: 1rem;
+  --spacing-m: 2rem;
+  --spacing-l: 3rem;
+}
+
+/* Spacing utility classes */
+.mt-xs {
+  margin-top: var(--spacing-xs);
+}
+
+.mb-xs {
+  margin-bottom: var(--spacing-xs);
+}
+
+.mt-s {
+  margin-top: var(--spacing-s);
+}
+
+.mb-s {
+  margin-bottom: var(--spacing-s);
+}
+
+.mt-m {
+  margin-top: var(--spacing-m);
+}
+
+.mb-m {
+  margin-bottom: var(--spacing-m);
+}
+
+.mt-l {
+  margin-top: var(--spacing-l);
+}
+
+.mb-l {
+  margin-bottom: var(--spacing-l);
+}
+
+/* Color utility classes */
+.color-primary {
+  color: var(--color-primary);
+}
+
+.bg-primary {
+  background-color: var(--color-primary);
+}
+
+.color-secondary {
+  color: var(--color-secondary);
+}
+
+.bg-secondary {
+  background-color: var(--color-secondary);
+}
+
+.color-accent {
+  color: var(--color-accent);
+}
+
+.bg-accent {
+  background-color: var(--color-accent);
+}
